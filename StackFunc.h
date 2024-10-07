98583e675178723c3fa919461c58de5b9284cc20
artemlobachev <lob_art@mail.ru>
2024-10-07 16:20:26 +0300

    .

--- StackFunc.h
+++
@@ -1,7 +0,0 @@
-#ifndef STACK_FUNCTIONS_H
-#define STACK_FUNCTIONS_H
-#include "stack.h"
-
-ErrorCode StackRealloc(Stack *SomeStack, int mode);
-Stack_t *MyRealloc(Stack *SomeStack, size_t ReallocMemory);
-#endif
